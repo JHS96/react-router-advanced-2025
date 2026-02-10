8e19e196032cf59c2b1fa873dfe1b2d19dd8d3a0
JHS96 <jschaper@hotmail.co.za>
2026-02-10 15:57:16 +0200

    Output list of dummy events with links to details page

--- frontend/src/pages/EventDetails.js
+++ frontend/src/pages/EventDetails.js
@@ -1,5 +1,14 @@
+import { useParams } from 'react-router-dom';
+
 function EventDetailsPage() {
-  return <h1>Event Details Page</h1>;
+  const params = useParams();
+
+  return (
+    <>
+      <h1>Event Details Page</h1>
+      <h3>Event ID: {params.id}</h3>
+    </>
+  );
 }
 
 export default EventDetailsPage;
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,5 +1,34 @@
+import EventsList from '../components/EventsList';
+
+const events = [
+  {
+    id: 'e1',
+    image: '',
+    title: 'Awesome Event 1',
+    date: new Date('2022-03-25').toDateString(),
+  },
+  {
+    id: 'e2',
+    image: '',
+    title: 'Awesome Event 2',
+    date: new Date('2025-04-26').toDateString(),
+  },
+  {
+    id: 'e3',
+    image: '',
+    title: 'Awesome Event 3',
+    date: new Date('2025-06-21').toDateString(),
+  },
+  {
+    id: 'e4',
+    image: '',
+    title: 'Awesome Event 4',
+    date: new Date('2026-09-07').toDateString(),
+  },
+];
+
 function EventsPage() {
-  return <h1>Events Page</h1>;
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
