a1ef905ca0c497da3131762c9580bdc54a7e057d
JHS96 <jschaper@hotmail.co.za>
2026-02-10 15:16:44 +0200

    RootLayout & MainNavigation added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,5 @@
 // Challenge / Exercise
 
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
 // 6. Output a list of dummy events to the EventsPage
 //    Every list item should include a link to the respective EventDetailPage
 // 7. Output the ID of the selected event on the EventDetailPage
@@ -15,13 +12,20 @@ import EventsPage from './pages/Events';
 import EventDetails from './pages/EventDetails';
 import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
+import RootLayout from './pages/Root';
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: 'events/', element: <EventsPage /> },
-  { path: 'events/:id', element: <EventDetails /> },
-  { path: 'events/new', element: <NewEventPage /> },
-  { path: 'events/:id/edit', element: <EditEventPage /> },
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'events/', element: <EventsPage /> },
+      { path: 'events/:id', element: <EventDetails /> },
+      { path: 'events/new', element: <NewEventPage /> },
+      { path: 'events/:id/edit', element: <EditEventPage /> },
+    ],
+  },
 ]);
 
 function App() {
